f4bd66358248c892f8d68db1c673fb86c6738ddd
Artur Luis <43670562+ArturLuisOliveira@users.noreply.github.com>
2023-11-17 14:57:46 -0300

    add converter slice logic

--- src/features/unit-converter/converterSlice.ts
+++ src/features/unit-converter/converterSlice.ts
@@ -1,5 +1,4 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export type Fact = {
   from: string;
@@ -16,7 +15,7 @@ export type Query = {
 export interface UnitConverterState {
   facts: Fact[];
   queryAnswer?: number;
-};
+}
 
 const initialState: UnitConverterState = {
   facts: [],
@@ -24,14 +23,49 @@ const initialState: UnitConverterState = {
 };
 
 export const converterSlice = createSlice({
-  name: 'converter',
+  name: "converter",
   initialState,
   reducers: {
     addFact: (state, action: PayloadAction<Fact>) => {
       // TODO: implement
+      return { ...state, facts: [...state.facts, action.payload] };
     },
     convertUnit: (state, action: PayloadAction<Query>) => {
       // TODO: implement
+      const { from, to, value } = action.payload;
+
+      //in-order case
+      const fact = state.facts.find((f) => f.from === from && f.to === to);
+      const queryAnswer = fact ? value * fact.ratio : undefined;
+      if (fact) return { ...state, queryAnswer };
+
+      //inverse case
+      const inverseFact = state.facts.find(
+        (f) => f.from === to && f.to === from
+      );
+      const inverseQueryAnswer = inverseFact
+        ? value / inverseFact.ratio
+        : undefined;
+      if (inverseFact) return { ...state, queryAnswer: inverseQueryAnswer };
+
+      //units with intermediate
+      //todo: improve this, currently it only accepts only one intermediate
+      const fromFacts = state.facts.filter((f) => f.from === from);
+      const toFacts = state.facts.filter((f) => f.to === to);
+      const intermediate = fromFacts.find((f) =>
+        toFacts.some((t) => t.from === f.to)
+      );
+      if (!intermediate) return { ...state, queryAnswer: undefined };
+      const intermediateFact = state.facts.find(
+        (f) => f.from === intermediate.to && f.to === to
+      );
+      const intermediateQueryAnswer = intermediateFact
+        ? value * intermediate.ratio * intermediateFact.ratio
+        : undefined;
+      if (intermediateFact)
+        return { ...state, queryAnswer: intermediateQueryAnswer };
+
+      return state;
     },
   },
 });
