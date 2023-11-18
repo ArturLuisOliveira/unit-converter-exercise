4d4dada5f8761dc569efa301ce65165350cc4bc3
Artur Luis <43670562+ArturLuisOliveira@users.noreply.github.com>
2023-11-18 16:42:58 -0300

    add graph class

---
+++ src/features/unit-converter/helpers/Graph.test.ts
@@ -0,0 +1,86 @@
+import { Graph, breadthFirstSearch } from "./Graph";
+
+describe("Graph", () => {
+  describe("given a graph", () => {
+    const graph = new Graph();
+
+    beforeEach(() => {
+      graph.addVertex({ value: "km" });
+      graph.addVertex({ value: "m" });
+      graph.addVertex({ value: "cm" });
+      graph.addVertex({ value: "mm" });
+      graph.addVertex({ value: "mile" });
+
+      graph.addEdge({
+        from: "km",
+        to: "m",
+        data: { value: 1000, operand: "*" },
+      });
+      graph.addEdge({
+        from: "m",
+        to: "km",
+        data: { value: 100, operand: "/" },
+      });
+      graph.addEdge({
+        from: "m",
+        to: "cm",
+        data: { value: 100, operand: "*" },
+      });
+      graph.addEdge({
+        from: "cm",
+        to: "m",
+        data: { value: 100, operand: "/" },
+      });
+      graph.addEdge({
+        from: "cm",
+        to: "mm",
+        data: { value: 10, operand: "*" },
+      });
+      graph.addEdge({
+        from: "mm",
+        to: "cm",
+        data: { value: 10, operand: "/" },
+      });
+      graph.addEdge({
+        from: "mile",
+        to: "km",
+        data: { value: 1.609344, operand: "*" },
+      });
+      graph.addEdge({
+        from: "km",
+        to: "mile",
+        data: { value: 1.609344, operand: "/" },
+      });
+    });
+
+    test("should return the correct graph distances", () => {
+      const { distances } = breadthFirstSearch({
+        graph,
+        startVertexValue: "km",
+        callback: (vertex) => {},
+      });
+
+      expect(distances.get("km")).toBe(0);
+      expect(distances.get("m")).toBe(1);
+      expect(distances.get("cm")).toBe(2);
+      expect(distances.get("mm")).toBe(3);
+    });
+
+    test("should return the correct graph predecessors", () => {
+      const { predecessors } = breadthFirstSearch({
+        graph,
+        startVertexValue: "km",
+        callback: (vertex) => {},
+      });
+
+      expect(predecessors.get("km")).toBe(null);
+      expect(predecessors.get("m")).toBe("km");
+      expect(predecessors.get("cm")).toBe("m");
+      expect(predecessors.get("mm")).toBe("cm");
+    });
+
+    afterEach(() => {
+      graph.clear();
+    });
+  });
+});
---
+++ src/features/unit-converter/helpers/Graph.ts
@@ -0,0 +1,86 @@
+export class Graph<VertexValue, EdgeData> {
+  equalityFunction = (a: VertexValue, b: VertexValue) => a == b;
+  vertices: Set<Vertex<VertexValue>> = new Set();
+  adjacencyList: Map<VertexValue, Map<VertexValue, EdgeData>> = new Map();
+
+  constructor(equalityFunction?: (a: VertexValue, b: VertexValue) => boolean) {
+    if (equalityFunction != null) this.equalityFunction = equalityFunction;
+  }
+
+  addVertex(vertex: Vertex<VertexValue>) {
+    this.vertices.add(vertex);
+    this.adjacencyList.set(vertex.value, new Map());
+  }
+
+  addEdge({
+    from,
+    to,
+    data,
+  }: {
+    from: VertexValue;
+    to: VertexValue;
+    data: EdgeData;
+  }) {
+    const neighbors = this.adjacencyList.get(from);
+    neighbors!.set(to, data);
+  }
+
+  clear() {
+    this.vertices = new Set();
+    this.adjacencyList = new Map();
+    this.equalityFunction = (a: VertexValue, b: VertexValue) => a == b;
+  }
+}
+
+export class Vertex<T> {
+  value: T;
+
+  constructor(value: T) {
+    this.value = value;
+  }
+}
+
+type Color = "white" | "grey" | "black";
+
+export const breadthFirstSearch = <VertexValue, EdgeData>({
+  graph,
+  callback,
+  startVertexValue,
+}: {
+  graph: Graph<VertexValue, EdgeData>;
+  startVertexValue: VertexValue;
+  callback: (vertex: VertexValue) => void;
+}) => {
+  const vertices = graph.vertices;
+  const adjList = graph.adjacencyList;
+  const distances = new Map<VertexValue, number>();
+  const predecessors = new Map<VertexValue, VertexValue | null>();
+  const colors = new Map<VertexValue, Color>();
+  for (const vertex of vertices) {
+    colors.set(vertex.value, "white");
+    distances.set(vertex.value, 0);
+    predecessors.set(vertex.value, null);
+  }
+  const queue: VertexValue[] = [startVertexValue];
+  while (queue.length) {
+    const current = queue.pop();
+    const neighbors = adjList.get(current as VertexValue);
+    colors.set(current!, "grey");
+
+    for (const [neighbor] of neighbors!) {
+      if (colors.get(neighbor) === "white") {
+        queue.unshift(neighbor);
+        distances.set(neighbor, distances.get(current!)! + 1);
+        predecessors.set(neighbor, current!);
+        colors.set(neighbor, "grey");
+      }
+    }
+    colors.set(current!, "black");
+    callback(current!);
+  }
+
+  return {
+    distances,
+    predecessors,
+  };
+};
